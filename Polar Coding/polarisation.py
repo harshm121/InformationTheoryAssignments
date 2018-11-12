b98637908ed2f036301320ee8746d25315580bf7
Harsh Maheshwari <harshm121@gmail.com>
2018-11-12 12:40:23 +0530

    Encoding done for polar coding

--- Polar Coding/polarisation.py
+++ Polar Coding/polarisation.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 I = []
-alpha = 0.4
+alpha = 0.7
 I.append(1 - alpha)
 N = 1
 Ilist = []
@@ -24,21 +24,31 @@ for n in range(1, 31):
 # 	plt.savefig('./figures/'+str(i)+'.png')	
 # 	plt.clf()
 
-Iless = []
-Imore = []
+# Iless = []
+# Imore = []
 # n = []
 # x = 1
-for i in range(len(Ilist)):
-	I = Ilist[i]
-	Iless.append(((sum([I[j]<0.01 for j in range(len(I))]*1))+0.)/len(I)*100)
-	Imore.append(((sum([I[j]>0.99 for j in range(len(I))]*1))+0.)/len(I)*100)
+# for i in range(len(Ilist)):
+# 	I = Ilist[i]
+# 	Iless.append(((sum([I[j]<0.01 for j in range(len(I))]*1))+0.)/len(I)*100)
+# 	Imore.append(((sum([I[j]>0.99 for j in range(len(I))]*1))+0.)/len(I)*100)
 	# n.append(x)
 	# x*=2
 
 # plt.plot(n, Iless)
 # plt.plot(n, Imore)
 # plt.legend(["number of channels with I<0.2", "number of channels with I>0.8"])
-p1 = plt.bar(range(1, len(Ilist)+1), Iless, 0.35, )
-p2 = plt.bar(range(1, len(Ilist)+1), Imore,0.35, bottom = Iless)
-plt.xticks(range(len(Ilist)),range(len(Ilist)) )
-plt.savefig("./figures/thisIsNiceBar.jpg")
+# p1 = plt.bar(range(1, len(Ilist)+1), Iless, 0.35, )
+# p2 = plt.bar(range(1, len(Ilist)+1), Imore,0.35, bottom = Iless)
+# plt.xticks(range(len(Ilist)),range(len(Ilist)) )
+# plt.savefig("./figures/thisIsNiceBar.jpg")
+
+
+for i in range(len(Ilist)):
+	x = range(1, len(Ilist[i])+1)
+	y = Ilist[i]
+	plt.figure(figsize=(22,22))
+	plt.scatter(x, y, c='r', marker= '*')
+	plt.savefig('./figures/'+str(i+1)+'.png')
+	plt.clf()
+	
